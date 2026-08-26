3cdf96939fa01a85ae98ffa75b8282b2e7bb295d
usinwt <76551874+usinwt@users.noreply.github.com>
2026-08-26 22:38:34 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -54,7 +54,7 @@ PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-app-passwall"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-banip-zh-cn"
 #25.12
